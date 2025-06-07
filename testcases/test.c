c85da02a8cf9d5aa2b6c75fcdfa69c6587b32ff0
D0CTT <danielhgt4@gmail.com>
2025-06-07 01:01:41 -0600

    [ADD] Testcases used to test the app functionality

---
+++ testcases/test.c
@@ -0,0 +1,15 @@
+#pragma addr 4000;
+int main() {
+    int x;
+    int y;
+    int z;
+    int m;
+    m = 0;
+    x = 5;
+    y = 7;
+    z = x + y;
+    while (z > 6) {
+        m = m + 5;
+        z = z - 2;
+    }
+}
